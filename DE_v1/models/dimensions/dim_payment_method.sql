5dd7d276c414fd3c2fe58facd24e9d64dcb98a0d
Amir Hakim <amirhakim@Amirs-MacBook-Air-2.local>
2026-04-27 22:00:52 +0800

    dim_payment_method under dimension models.

---
+++ DE_v1/models/dimensions/dim_payment_method.sql
@@ -0,0 +1,30 @@
+-- models/dimensions/dim_payment_method.sql
+
+with distinct_payment_methods as (
+    select distinct
+        trim(payment_method) as payment_method_name
+    from {{ ref('silver_curated') }}
+    where payment_method is not null
+    and trim(payment_method) <> ''
+),
+
+numbered_payment_methods as (
+    select
+        row_number() over (order by payment_method_name) as payment_method_key,
+        payment_method_name,
+        false as is_unknown
+    from distinct_payment_methods
+)
+
+select
+    -1 as payment_method_key,
+    'Unknown Payment Method'::text as payment_method_name,
+    true as is_unknown
+
+union all
+
+select
+    payment_method_key,
+    payment_method_name,
+    is_unknown
+from numbered_payment_methods
