3668c0e017e5595cfd80cd6bd2f6e66ae7f42013
Amir Hakim <amirhakim@Amirs-MacBook-Air-2.local>
2026-04-28 10:02:58 +0800

    Added dim_location and test for all dim table

---
+++ DE_v1/models/dimensions/dim_location.sql
@@ -0,0 +1,30 @@
+-- models/dimensions/dim_location.sql
+
+  with distinct_locations as (
+      select distinct
+          trim(location) as location_name
+      from {{ ref('silver_curated') }}
+      where location is not null
+        and trim(location) <> ''
+  ),
+
+  numbered_locations as (
+      select
+          row_number() over (order by location_name) as location_key,
+          location_name,
+          false as is_unknown
+      from distinct_locations
+  )
+
+  select
+      -1 as location_key,
+      'Unknown Location'::text as location_name,
+      true as is_unknown
+
+  union all
+
+  select
+      location_key,
+      location_name,
+      is_unknown
+  from numbered_locations
