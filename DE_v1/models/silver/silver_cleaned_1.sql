c35592a1b3d0047baf328b2063964a816e3749df
Amir Hakim <amirhakim@Amirs-MacBook-Air-2.local>
2026-04-30 11:53:32 +0800

    Update ingestion code to be in sql query instead of dataframe insert. removed strict tests on source table. fixed new source column case styles

--- DE_v1/models/silver/silver_cleaned_1.sql
+++ DE_v1/models/silver/silver_cleaned_1.sql
@@ -1,11 +1,11 @@
--- rename column headers to be in snake case
+-- source already uses snake_case column names
 SELECT 
-    "transaction id"      AS transaction_id,
-    "item"                AS item,
-    "quantity"            AS quantity,
-    "price per unit"      AS price_per_unit,
-    "total spent"         AS total_spent,
-    "payment method"      AS payment_method,
-    "location"            AS location,
-    "transaction date"    AS transaction_date
+    transaction_id      AS transaction_id,
+    item                AS item,
+    quantity            AS quantity,
+    price_per_unit      AS price_per_unit,
+    total_spent         AS total_spent,
+    payment_method      AS payment_method,
+    location            AS location,
+    transaction_date    AS transaction_date
 FROM {{ ref('bronze_cafe_sales') }}
