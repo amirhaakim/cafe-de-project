e1f21932fa5086a62030d1f646044d6d78dc347b
Amir Hakim <amirhakim@Amirs-MacBook-Air-2.local>
2026-05-25 16:01:47 +0800

    Updated ingestion_auto.py

--- orchestration/ingestion_auto.py
+++ orchestration/ingestion_auto.py
@@ -8,14 +8,14 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 # Call ingestion code from ingestion folder after reaching 2 levels higher from this file dir
 from ingestion import data_ingestion_updated
 
-PATH = data_ingestion_updated
+ingestion = data_ingestion_updated
 
-@task 
+@task
 def run_ingestion():
-    engine = PATH.get_engine()
-    PATH .check_postgres_connection(engine)
-    df = PATH.load_csv(PATH.CSV_PATH)
-    PATH.ingest_to_postgres(df,engine)
+    engine = ingestion.get_engine()
+    ingestion.check_postgres_connection(engine)
+    df = ingestion.load_csv(ingestion.CSV_PATH)
+    ingestion.ingest_to_postgres(df, engine)
     engine.dispose()
 
 @flow
